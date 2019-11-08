3dee38b3b89b43dfbafbfabde1f9fe22c322edcd
pandyshwallee <andreaar1605@gmail.com>
2019-11-07 21:07:28 -0600

    Agregué documentación, favor de revisar la misma.

--- prestamo.py
+++ prestamo.py
@@ -2,7 +2,10 @@ from enum import Enum
 from datetime import date
 from datetime import datetime
 from datetime import timedelta
-
+"""Clase Prestamo, crea objetos de tipo Prestamo cuyos atributos
+son el tipo de prestamo (se elige mediante enumeración), la fecha de 
+prestamo, el usuario a quien se le presta, el numero de ejemplar que
+se presta."""
 class TipoPrestamo(Enum):
 	prestamo_regular = 1 # dos semanas
 	prestamo_rapido = 2 # dos dias
@@ -11,7 +14,7 @@ class Prestamo():
 	def __init__(self, tipo_prestamo, usuario, ejemplar):
 		self.__tipo_prestamo = TipoPrestamo(tipo_prestamo).name
 		self.__fecha_prestamo = date.today()
-		if TipoPrestamo(tipo_prestamo) == 1:
+		if TipoPrestamo(tipo_prestamo) == 1: #por que se sumo aqui
 			self.__fecha_devuelta = self.__fecha_prestamo + timedelta(weeks=2)
 		else:
 			self.__fecha_devuelta = self.__fecha_prestamo + timedelta(days=2)
@@ -22,21 +25,29 @@ class Prestamo():
 		self.__ejemplar.set_disponibilidad(False)
 
 	def get_tipo_prestamo(self):
+		"""Metodo que devuelve el tipo de prestamo."""
 		return self.__tipo_prestamo
 
 	def get_fecha_prestamo(self):
+		"""Metodo que devuelve la fecha de prestamo."""
 		return self.__fecha_prestamo.strftime('%d-%m-%Y')
 
 	def get_fecha_devuelta(self):
+		"""Metodo que devuelve la fecha de regreso del libro, 
+		calculada con base en la fecha de prestamo y el tipo de prestamo."""
 		return self.__fecha_devuelta.strftime('%d-%m-%Y')
 
 	def get_usuario(self):
+		"""Metodo que devuelve el id del usuario."""
 		return self.__usuario.get_id()
 
 	def get_libro(self):
+		"""Metodo que devuelve el titulo del ejemplar prestado."""
 		return self.__ejemplar.get_titulo()
 
 	def terminar_prestamo(self):
+		"""Metodo que termina el prestamo de un libro, ademas multa al usuario
+		si se regreso el libro despues de tiempo."""
 		usuario.dec_libros_en_prestamo()
 		libro.set_disponibilidad(True)
 		if self.__fecha_devuelta <= date.today():
@@ -45,4 +56,5 @@ class Prestamo():
 			return True #SÃ­ hay multa
 
 	def __str__(self):
-		return "Libro: {}, Id usuario: {} \nFecha Salida: {}\tFecha Entrega: {}".format(self.get_libro(), self.get_usuario(), self.get_fecha_prestamo(), self.get_fecha_devuelta())
+		"""Metodo toString, imprime la informacion basica del prestamo."""
+		return "Libro: {}, ID usuario: {} \nFecha de salida: {}\tFecha de entrega: {}".format(self.get_libro(), self.get_usuario(), self.get_fecha_prestamo(), self.get_fecha_devuelta())
--- usuario.py
+++ usuario.py
@@ -1,5 +1,10 @@
-
+"""Clase Usuario, crea objetos de tipo Usuario
+que tiene como atributos un nombre, un id unico, 
+la cantidad de libros en préstamo y además lleva la cuenta
+de cuantos usuarios hay registrados hasta el momento."""
+	
 class Usuario():
+	
 	__cuenta = 0
 	def __init__(self, nombre):
 		self.__nombre = nombre
@@ -8,18 +13,32 @@ class Usuario():
 		self.__libros_en_prestamo = 0
 
 	def get_nombre(self):
+		"""Metodo que devuelve el nombre del usuario"""
+		
 		return self.__nombre
 
 	def get_id(self):
+		"""Metodo que devuelve el id unico del usuario"""
+		
 		return self.__id_user
 
 	def get_libros_en_prestamo(self):
+		"""Metodo que devuelve la cantidad de 
+		libros en prestamo"""
+		
 		return self.__libros_en_prestamo
 
 	def add_libros_en_prestamo(self):
+		"""Metodo que añade un 1 a la cantidad de libros
+		en prestamo del usuario cada que al 
+		usuario se le presta un libro"""
+		
 		self.__libros_en_prestamo += 1
 		return self.__libros_en_prestamo
 
 	def dec__libros_en_prestamo(self):
+		"""Metodo que decrementa en 1 la cantidad de libros
+		en prestamo cada que el usuario devuelve un libro"""
+		
 		self.__libros_en_prestamo -= 1
 		return self.__libros_en_prestamo
